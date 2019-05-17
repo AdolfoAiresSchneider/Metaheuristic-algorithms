c5888688839547e78b1e5a1b962c52ada8f5978b
AdolfoAiresSchneider <adolfo.aires@gmail.com>
2019-05-17 19:52:58 -0300

    TS - TSP

--- Tabu search/TSP/tabu_search.py
+++ Tabu search/TSP/tabu_search.py
@@ -3,64 +3,55 @@ import random
 
 class Solution:
   
-  path = []
-  fitness = 0
-  N = 0
-  
+	# Update the cost of the solution
   def update_fitness(self, Graph):
     self.fitness = 0
-    n = len(self.path)
-    for i in range(n-1):
-      u = self.path[i]
+    for i in range(self.N):
+      u = self.permutation[i]
       for edge in Graph[u]:
         v, cost = edge[1], edge[0]
-        if v == self.path[i+1]:
+        if v == self.permutation[i+1]:
           self.fitness += cost
           break
        
-  def copy(self, other):
-    self.fitness = other.fitness
-    self.path = copy.copy(other.path)
-  
-  # generate a greed solutuion(not the best one)
+  # Generate a greed solutuion(not the best one)
   def __init__(self, n = 0, source = 0, Graph = []):
     if n == 0:
       return
-    self.path.clear()
-    self.N = n
+    self.permutation = [] 		# Solution
+    self.N = n     		# Number of vertices
+    self.fitness = 0	# Quality of the solution
+    
     cur_node, self.fitness = source, 0
-    while cur_node not in self.path:
+    while cur_node not in self.permutation:
       for edge in Graph[cur_node]:
         v = edge[1]
-        if v not in self.path:
-          self.path.append(cur_node)
+        if v not in self.permutation:
+          self.permutation.append(cur_node)
           cur_node = v
           break
-      if len(self.path) == self.N-1:
+      if len(self.permutation) == self.N-1:
         break
     
-    self.path.append(cur_node)
-    self.path.append(source)
+    self.permutation.append(cur_node), self.permutation.append(source)
     self.update_fitness(Graph)
-      
+  
+  # Generate the neighborhood of this solution by randomly swapping two citys
   def gen_neighbors(self, Graph):
     neighbors = []
-    n = len(Graph)-1
-    for i in range(1, n-1):
-      j = random.randint(i+1, n-1)
-      new_sol = Solution()
-      new_sol.copy(self)
-      v1, v2 = self.path[i], self.path[j]
-      new_sol.path[i], new_sol.path[j] = v2, v1
-      new_sol.update_fitness(Graph)
-            
-      if new_sol not in neighbors:
-        neighbors.append((new_sol, (v1, v2)))
+    for i in range(1, self.N-1):
+      j = random.randint(i+1, self.N-1)
+      y = copy.deepcopy(self)
+      v1, v2 = self.permutation[i], self.permutation[j]
+      y.permutation[i], y.permutation[j] = v2, v1
+      y.update_fitness(Graph)
+      neighbors.append((y, (v1, v2)))
 
-    neighbors.sort(key=lambda x : x[0].fitness)
+    neighbors.sort(key = lambda x : x[0].fitness)
     return neighbors
-  pass	
-  
+  pass # End of class definition
+
+# Build the graph of the TSP instance
 def build_graph(arq):
   n = int(arq.readline())
   Graph = [[] for i in range(n+1)]
@@ -72,13 +63,9 @@ def build_graph(arq):
 	  Graph[u].sort()
   return Graph
 
-def tabu_search(first_sol, Graph, max_it, tbl_size):
-  tabu_list = list()
-  cur_sol = Solution()
-  best_sol = Solution()
-  cur_sol.copy(first_sol)
-  best_sol.copy(cur_sol)
-  
+# Tabu search method - Local search starting at the fisrt_sol maintain a Tabu list of size tbl_size
+def tabu_search(cur_sol, Graph, max_it, tbl_size, tabu_list = list()):
+  best_sol = copy.deepcopy(cur_sol)
   cnt = 1
   while cnt <= max_it:
     cnt += 1
@@ -87,38 +74,38 @@ def tabu_search(first_sol, Graph, max_it, tbl_size):
       move, neighbor = new_sol[1], new_sol[0]
       if move in tabu_list or move[::-1] in tabu_list:
         if neighbor.fitness < cur_sol.fitness:
-          cur_sol.copy(neighbor)
+          cur_sol = copy.deepcopy(neighbor)
           tabu_list.append(move)
-          break # olhar isso 
+          break
       else:
         tabu_list.append(move)
-        cur_sol.copy(neighbor)
+        cur_sol = copy.deepcopy(neighbor)
         if cur_sol.fitness < best_sol.fitness:
-          best_sol.copy(cur_sol)
-        break # olhar isso 
+          best_sol = copy.deepcopy(neighbor)
+        break
     
     if len(tabu_list) > tbl_size:
       tabu_list.pop(0)
   
   return best_sol
 
-def run_ts(source, Graph, max_it, sz_tabu_list):
-  grid_sol = Solution(len(Graph)-1, source, Graph)
-  final_sol = tabu_search(grid_sol, Graph, max_it, sz_tabu_list)
+# Run the Tabu search method with a start city called source.
+# source = First and last city for the TSP tour 
+def run_ts(source, Graph, max_iterations, tabu_list_siz):
+  grid_sol = Solution(len(Graph)-1, source, Graph) 
+  final_sol = tabu_search(grid_sol, Graph, max_iterations, tabu_list_siz)
   return final_sol
-
-
-####################################  Execution
-
+  
+#Execution of the Tabu search method for all possible sources
 arq = open('in')
 Graph = build_graph(arq)
-arq.close()
-
 sols = []
+
 for i in range(1, 52):
   x = run_ts(i, Graph, 100, 10)
-  print([i, x.fitness, len(x.path)])
+  print([i, x.fitness, len(x.permutation)])
   sols.append(x)
   
-sols.sort(key=lambda x : x.fitness)
-print(sols[0].fitness, sols[0].path)
+sols.sort(key = lambda x : x.fitness)
+print(sols[0].fitness, sols[0].permutation)
+arq.close()
